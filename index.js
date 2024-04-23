0d7beb9a6f91e6cc77072cdb51eb8405bd573755
Bruno Chaves <chavesbruno699@gmail.com>
2024-04-23 10:49:10 -0300

    addToCart

--- index.js
+++ index.js
@@ -9,3 +9,41 @@ const cartCounter = document.getElementById("cart-count");
 const inputAdress = document.getElementById("adress");
 const addressWarn = document.getElementById("address-warn");
 
+const checkoutItems = [];
+
+// Close and open ModalCheckout
+cartBtn.addEventListener("click", () => {
+    cartModal.style.display = "flex";
+})
+cartModal.addEventListener("click", (event) => {
+    if(event.target === cartModal || event.target === closeModalBtn){
+        cartModal.style.display = "none"
+    }
+})
+
+// Add item to checkout
+menu.addEventListener("click", (event) => {
+    const parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+
+        addToCart(name, price)    
+    }
+})
+
+function addToCart(name, price){
+    const existItem = checkoutItems.find(item => item.name === name)
+
+    if(existItem){
+        existItem.quantity += 1;
+        return
+    }
+
+    checkoutItems.push({
+        name,
+        price,
+        quantity: 1
+    })
+}
